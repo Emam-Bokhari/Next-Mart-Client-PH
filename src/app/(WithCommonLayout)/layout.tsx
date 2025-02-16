01cf4b98c4837260ac61f2441b255fea01f9f1bf
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-16 23:02:35 +0600

    feat: basic layout setup

--- src/app/(WithCommonLayout)/layout.tsx
+++ src/app/(WithCommonLayout)/layout.tsx
@@ -1,5 +1,13 @@
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
 import { Fragment, ReactNode } from "react";
 
 export default function CommonLayout({ children }: { children: ReactNode }) {
-  return <Fragment>{children}</Fragment>;
+  return (
+    <Fragment>
+      <Navbar />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </Fragment>
+  );
 }
