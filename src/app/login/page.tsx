530d62433d98e20bbf1161919c6b22c757df3d3d
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-17 15:33:14 +0600

    feat: implement login form ui and functionality

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/modules/auth/login/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <LoginForm />
+    </div>
+  );
+}
