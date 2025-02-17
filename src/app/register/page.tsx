17827412ab923edfdc3d1fe5f5c12bcade039e20
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-17 10:21:07 +0600

    feat: create register form and zod validation

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/components/modules/auth/register/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <RegisterForm />
+    </div>
+  );
+}
