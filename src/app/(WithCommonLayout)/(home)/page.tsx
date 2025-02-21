c418bbbb61aec5edd367c926690a6a75ab7913b0
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-21 12:06:20 +0600

    feat: add product card section in home page

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Category from "@/components/modules/home/Category";
+import FeaturedProducts from "@/components/modules/home/FeaturedProducts";
 import HeroSection from "@/components/modules/home/HeroSection";
 import { Fragment } from "react";
 
@@ -7,6 +8,7 @@ export default function HomePage() {
     <Fragment>
       <HeroSection />
       <Category />
+      <FeaturedProducts />
     </Fragment>
   );
 }
