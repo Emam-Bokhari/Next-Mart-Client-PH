d45159ee0e589d6782b1d20df7d9d7f64fe7ad38
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-22 21:26:14 +0600

    feat: add product details page

---
+++ src/app/(WithCommonLayout)/products/[productId]/page.tsx
@@ -0,0 +1,24 @@
+import ProductBanner from "@/components/modules/products/banner";
+import ProductDetails from "@/components/modules/products/productDetails";
+import NMContainer from "@/components/ui/core/NMContainer";
+import { getSingleProduct } from "@/services/Product";
+
+export default async function ProductDetailsPage({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const { productId } = await params;
+
+  const { data: product } = await getSingleProduct(productId);
+
+  return (
+    <NMContainer>
+      <ProductBanner
+        title="Product Details"
+        path="Home - Products - Product Details"
+      />
+      <ProductDetails product={product} />
+    </NMContainer>
+  );
+}
